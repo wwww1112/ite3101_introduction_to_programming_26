672e127613912d59d52214f2ec5d9118908d16a6
wwww1112 <130917532+wwww1112@users.noreply.github.com>
2026-09-22 03:20:27 +0000

    [skip actions] lab/lab01/ch01_t01_hello_world.py

--- lab/lab01/ch01_t01_hello_world.py
+++ lab/lab01/ch01_t01_hello_world.py
@@ -1,2 +1,2 @@
 print("Hello, world!")
-print("Water-there is not a drop of water there!Were Niagara but a cataract of sand would you travel your thousand miles to see it?")#Python3
+print("Deep into distant woolands winds a mazy way")#Python3
