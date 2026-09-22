f8b356d71286c5aa276165cfcf1223a37c1f2fb8
wwww1112 <130917532+wwww1112@users.noreply.github.com>
2026-09-22 03:19:33 +0000

    [skip actions] lab/lab01/ch01_t01_hello_world.py

--- lab/lab01/ch01_t01_hello_world.py
+++ lab/lab01/ch01_t01_hello_world.py
@@ -1,2 +1,2 @@
 print("Hello, world!")
-print("Water-there is not a drop of water there!Were Niagara but a cataract of sand would you travel your thousand miles to see it?")
+print("Water-there is not a drop of water there!Were Niagara but a cataract of sand would you travel your thousand miles to see it?")#Python3
